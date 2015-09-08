ebf6cf1a81557df1f25b60e0c96bbfef190b24f2
ChengTiesheng <chengtiesheng@huawei.com>
2015-09-08 11:33:10 +0800

    Update

--- convert.go
+++ convert.go
@@ -19,7 +19,23 @@ import (
 	"os/exec"
 	"io/ioutil"
 	"path/filepath"
+	"encoding/json"
 )
+
+type ImageManifest struct {
+	ACVersion     string `json:"acVersion"`
+	ACKind        string `json:"acKind"`
+	Name          string `json:"name"`
+	Labels        string `json:"labels,omitempty"`
+	App           string `json:"app,omitempty"`
+	Annotations   string `json:"annotations,omitempty"`
+	Dependencies  string `json:"dependencies,omitempty"`
+	PathWhitelist string `json:"pathWhitelist,omitempty"`
+}
+
+func blankImageManifest() *ImageManifest {
+	return &ImageManifest{ACKind: "ImageManifest", ACVersion: "0.6.1+git", Name: "test"}
+}
 
 func runOCI2ACI(path string, flagDebug bool) error {
 	if flagDebug {
@@ -54,10 +70,26 @@ func createWorkDir() string {
 	return idir
 }
 
+func genManifest() *ImageManifest {
+	return blankImageManifest()
+}
+
 func convertProc(srcPath, dstPath string) error {
 	src := srcPath + "/rootfs"
 	if err := run(exec.Command("cp", "-rf", src, dstPath)); err != nil {
 		return err
 	}
+
+	m := genManifest()
+	fmt.Println(m)
+	
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	
+	manifestPath := dstPath + "/manifest"
+
+	ioutil.WriteFile(manifestPath, bytes, 0644)
 	return nil
 }
